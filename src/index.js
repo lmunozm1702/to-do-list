7ca8a3a4df4b12c2eb448b65249971216101e5a5
Luis Muñoz <lmunozm1702@gmail.com>
2022-11-01 12:25:03 -0300

    Add styling and FrontEnd behavior
    Update index.js, index.html and style.css to set the styling and behavior in click and change on objects (scope of deliverable 1).

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import _ from 'lodash';
 import './style.css';
 import sampleData from './sampleData.js';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
 class toDoItem {
   constructor(description = null, completed = false, index = -1) {
@@ -25,16 +29,91 @@ class toDoList {
     });
 
     orderedList.forEach(listItem => {
-      let taskToAdd = document.createElement('li');
-      taskToAdd.className = 'single-task';
-      taskToAdd.textContent = listItem.description;
-      parentDiv.appendChild(taskToAdd);
+      this.renderTask(parentDiv, listItem);
     });
   }
 
+  renderTask = (parentDiv, task) => {
+    let taskToAdd = document.createElement('li');
+    taskToAdd.className = 'single-task no-edition';
+    taskToAdd.id = `li-task-${task.index}`;
+    taskToAdd.onclick = () => {
+      this.setNoeditBackground();
+      taskToAdd.classList.replace('no-edition', 'edition');
+      document.querySelector(`#vertical-${task.index}`).classList.replace('visible', 'hidden');
+      document.querySelector(`#trash-${task.index}`).classList.replace('hidden', 'visible');
+    }
+    parentDiv.appendChild(taskToAdd);
 
+    let taskDivLeft = document.createElement('div');
+    taskDivLeft.id = `left-div-li-${task.index}`;
+    taskDivLeft.className = 'left-div-li';
+    taskToAdd.appendChild(taskDivLeft);
+
+    let taskIcon = document.createElement('i');
+    taskIcon.id = `square-${task.index}`;
+    taskIcon.className = 'fa-regular fa-square grey-icon';
+    task.completed ? taskIcon.classList.add('hidden') : taskIcon.classList.add('visible');
+    taskDivLeft.appendChild(taskIcon);
+
+    taskIcon = document.createElement('i');
+    taskIcon.id = `check-${task.index}`;
+    taskIcon.className = 'fa-solid fa-check green-icon';
+    task.completed ? taskIcon.classList.add('visible') : taskIcon.classList.add('hidden');
+    taskDivLeft.appendChild(taskIcon);
+
+    let taskDesc = document.createElement('input');
+    taskDesc.type = 'text';
+    taskDesc.className = 'li-input-text';
+    task.completed ? taskDesc.classList.add('text-completed') : taskIcon.classList.add('text-uncompleted');
+    taskDesc.id = `li-input-text-${task.index}`;
+    taskDesc.value = task.description;
+    taskDivLeft.appendChild(taskDesc);
+
+    let taskDivRight = document.createElement('div');
+    taskDivRight.id = `right-div-li-${task.index}`;
+    taskToAdd.appendChild(taskDivRight);
+
+    taskIcon = document.createElement('i');
+    taskIcon.id = `vertical-${task.index}`;
+    taskIcon.className = 'fa-solid fa-ellipsis-vertical grey-icon visible';
+    taskDivRight.appendChild(taskIcon);
+
+    taskIcon = document.createElement('i');
+    taskIcon.id = `trash-${task.index}`;
+    taskIcon.className = 'fa-regular fa-trash-can grey-icon hidden';
+    taskDivRight.appendChild(taskIcon);
+  }
+
+  addNewTask(parentDiv, task) {
+    task.index = this.index + 1;
+    this.index += 1;
+
+    this.taskList.push(task);
+    this.renderTask(parentDiv, task);
+  }
+
+  setNoeditBackground = () => {
+    this.taskList.forEach(listItem => {
+      document.querySelector(`#li-task-${listItem.index}`).classList.replace('edition', 'no-edition');
+      document.querySelector(`#vertical-${listItem.index}`).classList.replace('hidden', 'visible');
+      document.querySelector(`#trash-${listItem.index}`).classList.replace('visible', 'hidden');
+    });
+  }
 }
 
 let myList = new toDoList(sampleData, 3, "My first list");
 myList.renderTaskList(document.querySelector('#list-title-left'), document.querySelector('#to-do-list'));
+
+const newItemInput = document.querySelector("#new-item-input");
+newItemInput.addEventListener('change', () => {
+  let newTask = new toDoItem(newItemInput.value, false, myList.index);
+  myList.index += 1;
+  myList.addNewTask(document.querySelector('#to-do-list'), newTask);
+  newItemInput.value = '';
+})
+
+newItemInput.addEventListener('click', () => {
+  myList.setNoeditBackground();
+});
 
