df642da70f6298b70f844f3cc8893bcd236c7500
Luis Muñoz <lmunozm1702@gmail.com>
2022-11-01 21:05:47 -0300

    Add edit and remove functionalities
    Update index.js and todolist.js to incorporate remove item (with reindexation on the remaining tasks) and edit a task.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import ToDoItem from './todoitem.js';
 import ToDoList from './todolist.js';
 import './style.css';
-import sampleData from './sampleData.js';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 
-
-let storedTasks = JSON.parse(localStorage.getItem('ToDoList')) || [];
-let storedName = JSON.parse(localStorage.getItem('listName')) || 'Demo';
-
-const myList = new ToDoList(storedTasks, storedTasks.length, storedName);
+const storedTasks = JSON.parse(localStorage.getItem('ToDoList')) || [];
+const storedName = JSON.parse(localStorage.getItem('listName')) || 'Demo';
+const storedIndex = storedTasks.length;
+const myList = new ToDoList(storedTasks, storedIndex, storedName);
 myList.renderTaskList(document.querySelector('#list-title-left'), document.querySelector('#to-do-list'));
 
 const newItemInput = document.querySelector('#new-item-input');
 newItemInput.addEventListener('change', () => {
   const newTask = new ToDoItem(newItemInput.value, false, myList.index);
-  myList.index += 1;
+
   myList.addNewTask(document.querySelector('#to-do-list'), newTask);
   newItemInput.value = '';
 });
--- src/todolist.js
+++ src/todolist.js
@@ -63,26 +63,44 @@ class ToDoList {
     }
     taskDesc.id = `li-input-text-${task.index}`;
     taskDesc.value = task.description;
+    taskDesc.onchange = (element) => {
+      this.editTask(task.index, element.target.value);
+    };
     taskDivLeft.appendChild(taskDesc);
 
     const taskDivRight = document.createElement('div');
     taskDivRight.id = `right-div-li-${task.index}`;
     taskToAdd.appendChild(taskDivRight);
 
+    const divEllip = document.createElement('div');
+    divEllip.id = `ellip-${task.index}`;
+    taskDivRight.appendChild(divEllip);
+
     taskIcon = document.createElement('i');
     taskIcon.id = `vertical-${task.index}`;
     taskIcon.className = 'fa-solid fa-ellipsis-vertical grey-icon visible';
-    taskDivRight.appendChild(taskIcon);
+    divEllip.appendChild(taskIcon);
 
+    const divTrash = document.createElement('div');
+    divTrash.id = `div-trash-${task.index}`;
+    divTrash.onclick = () => {
+      this.removeTask(task.index);
+    };
+    taskDivRight.appendChild(divTrash);
     taskIcon = document.createElement('i');
     taskIcon.id = `trash-${task.index}`;
     taskIcon.className = 'fa-regular fa-trash-can grey-icon hidden';
-    taskDivRight.appendChild(taskIcon);
+    divTrash.appendChild(taskIcon);
   }
 
-  addNewTask(parentDiv, task) {
-    task.index = this.index + 1;
+  editTask = (index, value) => {
+    this.taskList[index].description = value;
+    localStorage.setItem('ToDoList', JSON.stringify(this.taskList));
+  }
 
+  addNewTask = (parentDiv, task) => {
+    task.index = this.index;
+    this.index += 1;
     this.taskList.push(task);
     this.renderTask(parentDiv, task);
     localStorage.setItem('ToDoList', JSON.stringify(this.taskList));
@@ -94,6 +112,28 @@ class ToDoList {
       document.querySelector(`#vertical-${listItem.index}`).classList.replace('hidden', 'visible');
       document.querySelector(`#trash-${listItem.index}`).classList.replace('visible', 'hidden');
     });
+  }
+
+  resetIndexes = () => {
+    let i = 0;
+    this.taskList.forEach((listItem) => {
+      listItem.index = i;
+      i += 1;
+    });
+    this.index = i;
+  }
+
+  removeTask = (listId) => {
+    this.taskList = this.taskList.filter(
+      (task) => task.index.toString() !== listId.toString(),
+    );
+    this.resetIndexes();
+    localStorage.setItem('ToDoList', JSON.stringify(this.taskList));
+    const lis = document.querySelectorAll('.single-task');
+    lis.forEach((element) => {
+      document.querySelector(`#${element.id}`).remove();
+    });
+    this.renderTaskList(document.querySelector('#list-title-left'), document.querySelector('#to-do-list'));
   }
 }
 
