d7c13e24c1119efd84c7d2d4566ca968b3434f63
Luis Muñoz <lmunozm1702@gmail.com>
2022-11-09 18:47:26 -0300

    Solve linter errors

--- src/todolist.test.js
+++ src/todolist.test.js
@@ -17,21 +17,21 @@ describe('add and remove function test', () => {
 
     myList.addNewTask(parentDiv, newTask);
     expect(myList.index).toBe(1);
-  })
+  });
 
   test('test dom for add', () => {
     const parentDivCount = document.querySelectorAll('#to-do-list li');
     expect(parentDivCount).toHaveLength(1);
-  })
+  });
 
   test('test local storage for add', () => {
     const storedTasks = JSON.parse(localStorage.getItem('ToDoList')) || [];
     expect(storedTasks).toHaveLength(1);
-  })
+  });
 
   test('remove item', () => {
-    document.body.innerHTML = "<div id='list-title-left'></div>"
-    document.body.innerHTML += "<div id='to-do-list'></div>"
+    document.body.innerHTML = "<div id='list-title-left'></div>";
+    document.body.innerHTML += "<div id='to-do-list'></div>";
     document.body.innerHTML += '<span id="red-badge" class="badge2"></span>';
     myList.removeTask(0);
     expect(myList.index).toBe(0);
@@ -40,10 +40,10 @@ describe('add and remove function test', () => {
   test('test dom for remove', () => {
     const parentDivCount = document.querySelectorAll('#to-do-list li');
     expect(parentDivCount).toHaveLength(0);
-  })
+  });
 
   test('test local storage for remove', () => {
     const storedTasks = JSON.parse(localStorage.getItem('ToDoList')) || [];
     expect(storedTasks).toHaveLength(0);
-  })
+  });
 });
